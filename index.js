a12d776dfa80da19d8fe020e07ae677017a75c5e
Jason Walsh <j@wal.sh>
2016-11-22 19:58:45 -0500

    Scaffolding for API
    https://github.com/iuse/seguranca/blob/master/entrega3/cipher/src/signature/Schnorr.java
    https://en.wikipedia.org/wiki/Schnorr_signature

---
+++ index.js
@@ -0,0 +1,54 @@
+/**
+ * Schnorr algorithm.
+ *
+ * Implementation of the Schnorr algorithm for digital signature.
+ * This class has methods to generate a key pair, sign, and verify signatures.
+ *
+ * https://github.com/iuse/seguranca/blob/master/entrega3/cipher/src/signature/Schnorr.java
+ */
+
+var x, y, p, q, g;
+
+var config = function(p, q, g)  {
+
+};
+
+/**
+ * Updates message
+ *
+ * @param M Message
+ */
+var update = function(M) {
+
+}
+
+/**
+ * Generates key pair from a user-defined password
+ *
+ * @param pw User-defined password
+ * @return Returns the public key
+ */
+var keyPairGen = function(pw) {
+
+};
+
+/**
+ * Signs message
+ *
+ * @return Returns the signed message
+ */
+
+var sign = function() {
+
+};
+
+/**
+ * Compares the signed message with the raw message
+ *
+ * @param sigma Signed message
+ * @param key Sender public key
+ * @return Returns true if the signature is valid
+ */
+var verify = function(sigma, key) {
+
+}
